f054fe23fb382ed1a071e5056f920fa99453612e
MUHAMMAD ZAHID RAMZAN <ramzanzahid356@gmail.com>
2025-06-30 08:52:44 +0500

    Update iris_logistic_regression.py

--- iris_logistic_regression.py
+++ iris_logistic_regression.py
@@ -51,3 +51,35 @@ print("\nAccuracy Score:",accuracy_score(y_test,y_pred))
 sample = [[5.1,3.5,1.4,0.2]]
 predicted_class = model.predict(sample)
 print("\nSample Prediction:",iris.target_names[predicted_class[0]])
+
+# Output
+# Dataset Preview:
+#    sepal length (cm)  sepal width (cm)  ...  target  flower_name
+# 0                5.1               3.5  ...       0       setosa
+# 1                4.9               3.0  ...       0       setosa
+# 2                4.7               3.2  ...       0       setosa
+# 3                4.6               3.1  ...       0       setosa
+# 4                5.0               3.6  ...       0       setosa
+
+# [5 rows x 6 columns]
+
+# Confusion Matrix:
+# [[10  0  0]
+#  [ 0  9  0]
+#  [ 0  0 11]]
+
+# Classification Report:
+#               precision    recall  f1-score   support
+
+#       setosa       1.00      1.00      1.00        10
+#   versicolor       1.00      1.00      1.00         9
+#    virginica       1.00      1.00      1.00        11
+
+#     accuracy                           1.00        30
+#    macro avg       1.00      1.00      1.00        30
+# weighted avg       1.00      1.00      1.00        30
+
+
+# Accuracy Score: 1.0
+
+# Sample Prediction: setosa
